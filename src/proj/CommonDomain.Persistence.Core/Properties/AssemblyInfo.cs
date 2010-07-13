3a3bb9e51a55a57baf1e70bf252f03274962253d
Oliver, Jonathan <joliver@localhost>
2010-07-13 10:35:20 -0600

    Signed all assemblies.

---
+++ src/proj/CommonDomain.Persistence.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CommonDomain.Persistence.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("9d28dd4a-e904-4758-bb59-b74df4c80e23")]
--- src/proj/CommonDomain/IAggregate.cs
+++ src/proj/CommonDomain/IAggregate.cs
@@ -10,6 +10,8 @@ namespace CommonDomain
 
 		void ApplyEvent(object @event);
 		ICollection GetUncommittedEvents();
+		void ClearUncommittedEvents();
+
 		IMomento GetSnapshot();
 	}
 }
