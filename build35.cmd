75c5fc99b1db2e8540fb45fffafd72debf904e2e
Oliver, Jonathan <jonathan.s.oliver42+git@gmail.com>
2010-11-06 20:05:46 -0600

    Adding .NET v3.5 and v4.0 build scripts.

--- build35.cmd
+++ build35.cmd
@@ -1,11 +1,12 @@
 @echo off
-SET PATH=%PATH%;C:\WINDOWS\Microsoft.NET\Framework\V3.5;
+SET FRAMEWORK_PATH=C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319
+SET PATH=%PATH%;%FRAMEWORK_PATH%;
 
 if not exist output ( mkdir output )
 
 echo Compiling
 msbuild /nologo /verbosity:quiet src/CommonDomain.sln /p:Configuration=Release /t:Clean
-msbuild /nologo /verbosity:quiet src/CommonDomain.sln /p:Configuration=Release
+msbuild /nologo /verbosity:quiet src/CommonDomain.sln /p:Configuration=Release /property:TargetFrameworkVersion=v3.5
 
 echo Merging
 
@@ -16,7 +17,7 @@ SET FILES_TO_MERGE=%FILES_TO_MERGE% "src/proj/CommonDomain.Persistence/bin/Relea
 SET FILES_TO_MERGE=%FILES_TO_MERGE% "src/proj/CommonDomain.Persistence.EventStore/bin/Release/CommonDomain.Persistence.EventStore.dll"
 SET FILES_TO_MERGE=%FILES_TO_MERGE% "src/proj/CommonDomain.Persistence.EventStore/bin/Release/EventStore.dll"
 
-bin\ilmerge-bin\ILMerge.exe /keyfile:src/CommonDomain.snk /v2 /xmldocs /out:output/CommonDomain.dll %FILES_TO_MERGE%
+bin\ilmerge-bin\ILMerge.exe /keyfile:src/CommonDomain.snk /xmldocs /v2 /out:output/CommonDomain.dll %FILES_TO_MERGE%
 
 echo Cleaning
 msbuild /nologo /verbosity:quiet src/CommonDomain.sln /p:Configuration=Release /t:Clean
