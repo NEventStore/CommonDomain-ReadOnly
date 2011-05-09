9f820c7a7efe4158276108084997ef951a03da82
Lars Andreas Ek <lars.andreas.ek@gmail.com>
2011-05-09 23:37:08 +0200

    RegistrationEventRouter now throws a human readable exception

--- src/proj/CommonDomain.Core/RegistrationEventRouter.cs
+++ src/proj/CommonDomain.Core/RegistrationEventRouter.cs
@@ -1,24 +1,32 @@
 namespace CommonDomain.Core
 {
-	using System;
-	using System.Collections.Generic;
+    using System;
+    using System.Collections.Generic;
 
-	public class RegistrationEventRouter<TEvent> : IRouteEvents<TEvent>
-	{
-		private readonly IDictionary<Type, Action<TEvent>> handlers = new Dictionary<Type, Action<TEvent>>();
+    public class RegistrationEventRouter<TEvent> : IRouteEvents<TEvent>
+    {
+        private readonly IDictionary<Type, Action<TEvent>> handlers = new Dictionary<Type, Action<TEvent>>();
+        private string _aggregateTypeName;
 
-		public virtual void Register<TEventMessage>(Action<TEventMessage> handler) where TEventMessage : TEvent
-		{
-			this.handlers[typeof(TEventMessage)] = @event => handler((TEventMessage)@event);
-		}
-		public virtual void Register(IAggregate aggregate)
-		{
-			// no-op
-		}
+        public virtual void Register<TEventMessage>(Action<TEventMessage> handler) where TEventMessage : TEvent
+        {
+            this.handlers[typeof(TEventMessage)] = @event => handler((TEventMessage)@event);
+        }
+        public virtual void Register(IAggregate aggregate)
+        {
+            _aggregateTypeName = aggregate.GetType().Name;
+        }
 
-		public virtual void Dispatch(object eventMessage)
-		{
-			this.handlers[eventMessage.GetType()]((TEvent)eventMessage);
-		}
-	}
+        public virtual void Dispatch(object eventMessage)
+        {
+            Action<TEvent> action;
+
+            if (!this.handlers.TryGetValue(eventMessage.GetType(), out action))
+            {
+                throw new HandlerForDomainEventNotFoundException(string.Format("Aggregate of type {0} raised and event of type {1}, but no handler was registered.", _aggregateTypeName, eventMessage.GetType().Name));
+            }
+
+            action((TEvent)eventMessage);
+        }
+    }
 }
