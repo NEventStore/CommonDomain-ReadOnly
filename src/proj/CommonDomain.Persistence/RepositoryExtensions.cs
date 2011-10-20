a0fcce0c7175429f506694c63b0821408d2bfc79
Jonathan Matheus <jmatheus@gmail.com>
2011-10-19 20:26:21 -0500

    Added extension for saving without headers

---
+++ src/proj/CommonDomain.Persistence/RepositoryExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommonDomain.Persistence
+{
+    public static class RepositoryExtensions
+    {
+         public static void Save(this IRepository repository, IAggregate aggregate, Guid commitId)
+         {
+             repository.Save(aggregate, commitId, a => {});
+         }
+    }
+}
