fbee824aa82ec27376cb02243ac6118978353b33
Dmytro Aleksandrov <alexandrov.dmitry@gmail.com>
2012-09-30 08:36:40 +0300

    Issue #28 - stream caching disabled

--- src/proj/CommonDomain.Persistence.EventStore/EventStoreRepository.cs
+++ src/proj/CommonDomain.Persistence.EventStore/EventStoreRepository.cs
@@ -6,11 +6,9 @@ namespace CommonDomain.Persistence.EventStore
 	using global::EventStore;
 	using global::EventStore.Persistence;
 
-	public class EventStoreRepository : IRepository, IDisposable
+	public class EventStoreRepository : IRepository
 	{
 		private const string AggregateTypeHeader = "AggregateType";
-		private readonly IDictionary<Guid, Snapshot> snapshots = new Dictionary<Guid, Snapshot>();
-		private readonly IDictionary<Guid, IEventStream> streams = new Dictionary<Guid, IEventStream>();
 		private readonly IStoreEvents eventStore;
 		private readonly IConstructAggregates factory;
 		private readonly IDetectConflicts conflictDetector;
@@ -25,26 +23,6 @@ namespace CommonDomain.Persistence.EventStore
 			this.conflictDetector = conflictDetector;
 		}
 
-		public void Dispose()
-		{
-			this.Dispose(true);
-			GC.SuppressFinalize(this);
-		}
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!disposing)
-				return;
-
-			lock (this.streams)
-			{
-				foreach (var stream in this.streams)
-					stream.Value.Dispose();
-
-				this.snapshots.Clear();
-				this.streams.Clear();
-			}
-		}
-
         public virtual TAggregate GetById<TAggregate>(Guid id) where TAggregate : class, IAggregate
         {
             return GetById<TAggregate>(id, int.MaxValue);
@@ -52,13 +30,15 @@ namespace CommonDomain.Persistence.EventStore
 
 	    public virtual TAggregate GetById<TAggregate>(Guid id, int versionToLoad) where TAggregate : class, IAggregate
 		{
-			var snapshot = this.GetSnapshot(id, versionToLoad);
-			var stream = this.OpenStream(id, versionToLoad, snapshot);
-			var aggregate = this.GetAggregate<TAggregate>(snapshot, stream);
+			var snapshot = GetSnapshot(id, versionToLoad);
+            using (var stream = OpenStream(id, versionToLoad, snapshot))
+            {
+                var aggregate = GetAggregate<TAggregate>(snapshot, stream);
 
-			ApplyEventsToAggregate(versionToLoad, stream, aggregate);
+                ApplyEventsToAggregate(versionToLoad, stream, aggregate);
 
-			return aggregate as TAggregate;
+                return aggregate as TAggregate;
+            }
 		}
 		private static void ApplyEventsToAggregate(int versionToLoad, IEventStream stream, IAggregate aggregate)
 		{
@@ -69,27 +49,17 @@ namespace CommonDomain.Persistence.EventStore
 		private IAggregate GetAggregate<TAggregate>(Snapshot snapshot, IEventStream stream)
 		{
 			var memento = snapshot == null ? null : snapshot.Payload as IMemento;
-			return this.factory.Build(typeof(TAggregate), stream.StreamId, memento, stream.CommittedHeaders);
+			return factory.Build(typeof(TAggregate), stream.StreamId, memento, stream.CommittedHeaders);
 		}
 		private Snapshot GetSnapshot(Guid id, int version)
 		{
-			Snapshot snapshot;
-			if (!this.snapshots.TryGetValue(id, out snapshot))
-				this.snapshots[id] = snapshot = this.eventStore.Advanced.GetSnapshot(id, version);
-
-			return snapshot;
+			return eventStore.Advanced.GetSnapshot(id, version);
 		}
 		private IEventStream OpenStream(Guid id, int version, Snapshot snapshot)
 		{
-			IEventStream stream;
-			if (this.streams.TryGetValue(id, out stream))
-				return stream;
-
-			stream = snapshot == null
-				? this.eventStore.OpenStream(id, 0, version)
-				: this.eventStore.OpenStream(snapshot, version);
-
-			return this.streams[id] = stream;
+			return snapshot == null
+				? eventStore.OpenStream(id, 0, version)
+				: eventStore.OpenStream(snapshot, version);
 		}
 
 		public virtual void Save(IAggregate aggregate, Guid commitId, Action<IDictionary<string, object>> updateHeaders)
@@ -97,38 +67,38 @@ namespace CommonDomain.Persistence.EventStore
 			var headers = PrepareHeaders(aggregate, updateHeaders);
 			while (true)
 			{
-				var stream = this.PrepareStream(aggregate, headers);
-				var commitEventCount = stream.CommittedEvents.Count;
+                using (var stream = PrepareStream(aggregate, headers))
+                {
+                    var commitEventCount = stream.CommittedEvents.Count;
 
-				try
-				{
-					stream.CommitChanges(commitId);
-					aggregate.ClearUncommittedEvents();
-					return;
-				}
-				catch (DuplicateCommitException)
-				{
-					stream.ClearChanges();
-					return;
-				}
-				catch (ConcurrencyException e)
-				{
-					if (this.ThrowOnConflict(stream, commitEventCount))
-						throw new ConflictingCommandException(e.Message, e);
+                    try
+                    {
+                        stream.CommitChanges(commitId);
+                        aggregate.ClearUncommittedEvents();
+                        return;
+                    }
+                    catch (DuplicateCommitException)
+                    {
+                        stream.ClearChanges();
+                        return;
+                    }
+                    catch (ConcurrencyException e)
+                    {
+                        if (ThrowOnConflict(stream, commitEventCount))
+                            throw new ConflictingCommandException(e.Message, e);
 
-					stream.ClearChanges();
-				}
-				catch (StorageException e)
-				{
-					throw new PersistenceException(e.Message, e);
-				}
+                        stream.ClearChanges();
+                    }
+                    catch (StorageException e)
+                    {
+                        throw new PersistenceException(e.Message, e);
+                    }
+                }
 			}
 		}
 		private IEventStream PrepareStream(IAggregate aggregate, Dictionary<string, object> headers)
 		{
-			IEventStream stream;
-			if (!this.streams.TryGetValue(aggregate.Id, out stream))
-				this.streams[aggregate.Id] = stream = this.eventStore.CreateStream(aggregate.Id);
+			IEventStream stream = eventStore.OpenStream(aggregate.Id, 0, int.MaxValue);
 
 			foreach (var item in headers)
 				stream.UncommittedHeaders[item.Key] = item.Value;
@@ -155,7 +125,7 @@ namespace CommonDomain.Persistence.EventStore
 		{
 			var committed = stream.CommittedEvents.Skip(skip).Select(x => x.Body);
 			var uncommitted = stream.UncommittedEvents.Select(x => x.Body);
-			return this.conflictDetector.ConflictsWith(uncommitted, committed);
+			return conflictDetector.ConflictsWith(uncommitted, committed);
 		}
 	}
 }
