fb7bb4d7c8f64f608b09ff4f2dad0bfe6cc26bd1
Lars Wilhelmsen <lars@sral.org>
2013-01-01 22:25:14 +0100

    Fixed indentation
    nitpicking.

--- src/proj/CommonDomain.Persistence/IRepository.cs
+++ src/proj/CommonDomain.Persistence/IRepository.cs
@@ -5,7 +5,7 @@ namespace CommonDomain.Persistence
 
 	public interface IRepository
 	{
-	    TAggregate GetById<TAggregate>(Guid id) where TAggregate : class, IAggregate;
+		TAggregate GetById<TAggregate>(Guid id) where TAggregate : class, IAggregate;
 		TAggregate GetById<TAggregate>(Guid id, int version) where TAggregate : class, IAggregate;
 		void Save(IAggregate aggregate, Guid commitId, Action<IDictionary<string, object>> updateHeaders);
 	}
